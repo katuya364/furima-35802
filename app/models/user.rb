844182b2afdd6414443f992f3db7eb0568d91276
katuya364 <konishi8439@gmail.com>
2021-06-24 22:10:54 +0900

    バリーデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name_kanji, presence: true
-  validates :last_name_kanji, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :first_name_kanji, presence: true, format: { with: /\A[一-龥ぁ-ん]/, message: "full-width characters"}
+  validates :last_name_kanji, presence: true, format: { with: /\A[一-龥ぁ-ん]/, message: "full-width characters" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "first_name_kana full-width katakana characters" }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "last_name_kana full-width katakana characters" }
   validates :birthday, presence: true
+  validates :password,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/, message: "Include both letters and numbers"}
 end
