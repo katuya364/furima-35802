439e7d91f3f63f2e93b6561514179ea4647a517e
katuya364 <konishi8439@gmail.com>
2021-06-25 17:59:42 +0900

    正規表現の追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :first_name_kanji, format: { with: /\A[一-龥ぁ-ん]/, message: 'full-width characters' }
-    validates :last_name_kanji,  format: { with: /\A[一-龥ぁ-ん]/, message: 'full-width characters' }
+    validates :first_name_kanji, format: { with: /\A[一-龥々ぁ-ん]+\z/, message: 'full-width characters' }
+    validates :last_name_kanji,  format: { with: /\A[一-龥々ぁ-ん]+\z/, message: 'full-width characters' }
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'first_name_kana full-width katakana characters' }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'last_name_kana full-width katakana characters' }
     validates :birthday
