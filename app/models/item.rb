02b659f5742d38a1a61e58e82bbe4352ac5f5a8f
katuya364 <konishi8439@gmail.com>
2021-06-28 14:14:23 +0900

    価格のバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,9 +12,8 @@ class Item < ApplicationRecord
     validates :image
     validates :item_name
     validates :item_description
-    with_options numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 } do
-    validates :price
-    end
+    validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Input half-width characters' } 
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range'} 
     with_options numericality: { other_than: 1 } do
       validates :category_id
       validates :condition_id
