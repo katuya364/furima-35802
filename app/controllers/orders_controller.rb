678f9ddda69b951b2815754fc32068c29919a02f
katuya364 <konishi8439@gmail.com>
2021-07-06 20:15:51 +0900

    orderコントローラーの記述

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,47 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: :index
+  before_action :solid_out, only: :index
+
+  def index
+    @order_shipping_address = OrderShippingAddress.new
+  end
+
+  def create
+    @order_shipping_address = OrderShippingAddress.new(order_params)
+    if @order_shipping_address.valid?
+      pay_item
+      @order_shipping_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    redirect_to root_path unless @item.user_id != current_user.id 
+  end
+
+  def solid_out
+    redirect_to root_path if @item.order.present?
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+end
